0625da434e561dfce50bd376f427aa0d944ef43e
akbarian <a.akbariyan@gmail.com>
2014-02-12 22:57:41 +0330

    Add CategorieTree jsf bean to project

---
+++ src/com/jprogrammers/jsf/beans/CategoriesTreeBean.java
@@ -0,0 +1,30 @@
+package com.jprogrammers.jsf.beans;
+
+import org.primefaces.model.DefaultTreeNode;
+import org.primefaces.model.TreeNode;
+
+import java.io.Serializable;
+
+/**
+ * Created by User on 12/02/14.
+ */
+public class CategoriesTreeBean implements Serializable {
+
+    private TreeNode root;
+
+    public CategoriesTreeBean() {
+        root = new DefaultTreeNode("root" , null);
+
+        TreeNode node0 = new DefaultTreeNode("Core Java" , root);
+        TreeNode node01 = new DefaultTreeNode("Annotations" , node0);
+        TreeNode node02 = new DefaultTreeNode("Swing" , node0);
+
+        TreeNode node1 = new DefaultTreeNode("Hibernate" , root);
+        TreeNode node2 = new DefaultTreeNode("JSF" , root);
+    }
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+}
